a77822f37e714ddf3615c496382abb06e935019f
liyiquan <liyiquan@topiaec.com>
2016-08-30 17:25:00 +0800

    基本任务清单完成

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,44 @@
 */
 
 use App\Task;
+use Illuminate\Http\Request;
 
+/**
+ * 显示所有任务
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * 增加新的任务
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * 删除一个已有任务
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
 });
